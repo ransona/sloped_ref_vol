b6264c82544c3587d8aa0fe9d5074df456254cb2
ransona <ranson.ad@gmail.com>
2025-02-03 17:32:39 +0100

    Update zref_mid_frame_acquire.m

--- zref_mid_frame_acquire.m
+++ zref_mid_frame_acquire.m
@@ -1,4 +1,4 @@
-function zref_mid_frame(animalID,ref_name,ch)
+function zref_mid_frame_acquire(animalID,ref_name,ch)
 if ~exist('animalID')
     error('No animal ID')
 end
@@ -16,67 +16,67 @@ frames_per_depth = 30;
 global hSI;
 
 
-        % filename root
-        stack_filename_stem = datestr(now);
-        stack_filename_stem = strrep(stack_filename_stem,':','_');
-        
-        % stack_filename_stem = [ref_name,'_','13-Apr-2023 14_34_52'];
-        
-        rawDataDir = fullfile('V:\Local_Repository',animalID,'refz',stack_filename_stem,'raw');
-        [ ~, ~ ] = mkdir(rawDataDir);
-        x = tic;
-        
-        logInitialState = hSI.hChannels.loggingEnable;
-        hSI.hMotionManager.enable=false;
-        hSI.hChannels.loggingEnable=true;
-        % start with the fast z profile open that you want to use with the scope
-        % in the zoom / zposition that you want to use
-        % channel to use
-        
-        
-        hSI.hScan2D.logFilePath = rawDataDir;
-        
-        % position fast z actuator at zero
-        % hSI.hFastZ.positionAbsolute = 0;
-        % zeros all motor positions
-        hSI.hMotors.setRelativeZero; % this line may change with SI version
-        % get z spacing of fast z planes
-        fast_z_step = hSI.hStackManager.stackZStepSize;
-        fast_z_slices = hSI.hStackManager.numSlices;
-        % calculate reference stack z positions
-        num_ref_planes = length(0:plane_spacing:fast_z_step);
-        ref_stack_z = -(total_range/2):plane_spacing:(total_range/2);
-        % set SI volumes to frames_per_depth
-        numVolumes_original = hSI.hStackManager.numVolumes;
-        hSI.hStackManager.numVolumes = frames_per_depth;
-        all_filenames = [];
-        for iDepth = 1:length(ref_stack_z)
-            disp(['Acquiring volume ',num2str(iDepth),' of ',num2str(length(ref_stack_z))]);
-            % set z position
-            hSI.hMotors.moveSample([0 0 ref_stack_z(iDepth)]);
-            
-            % set filename
-            % hSI.hScan2D.logFileStem = ['z_ref_',stack_filename_stem,'_',sprintf('%06d', ref_stack_z(iDepth)),'_'];
-            hSI.hScan2D.logFileStem = ['z_ref_',stack_filename_stem,'_',sprintf('%06d', iDepth),'_'];
-            
-            hSI.startGrab;
-            % drawnow;
-            % pause(3);
-            % wait for grab to be complete
-            while strcmp(hSI.acqState,'grab')
-                drawnow();
-            end
-        end
-        
+% filename root
+stack_filename_stem = datestr(now);
+stack_filename_stem = strrep(stack_filename_stem,':','_');
+stack_filename_stem = [ref_name,'_',stack_filename_stem];
 
-    
-disp(['Time taken = ',num2str(toc(x))]);
-if reg_only == false
-    % put settings back to start state
-    hSI.hStackManager.numVolumes = 99999999;
-    hSI.hChannels.loggingEnable=logInitialState;
-    hSI.hScan2D.logFileStem = '';
-    hSI.hScan2D.logFilePath = '';
-    % set motor to where it was
-    hSI.hMotors.moveSample([0 0 0]);
+rawDataDir = fullfile('V:\Local_Repository',animalID,'refz',stack_filename_stem,'raw');
+[ ~, ~ ] = mkdir(rawDataDir);
+x = tic;
+
+logInitialState = hSI.hChannels.loggingEnable;
+hSI.hMotionManager.enable=false;
+hSI.hChannels.loggingEnable=true;
+% start with the fast z profile open that you want to use with the scope
+% in the zoom / zposition that you want to use
+% channel to use
+
+
+hSI.hScan2D.logFilePath = rawDataDir;
+
+% position fast z actuator at zero
+% hSI.hFastZ.positionAbsolute = 0;
+% zeros all motor positions
+hSI.hMotors.setRelativeZero; % this line may change with SI version
+
+% get z spacing of fast z planes
+fast_z_step = hSI.hStackManager.stackZStepSize;
+fast_z_slices = hSI.hStackManager.numSlices;
+% calculate reference stack z positions
+num_ref_planes = length(0:plane_spacing:fast_z_step);
+ref_stack_z = -(total_range/2):plane_spacing:(total_range/2);
+% set SI volumes to frames_per_depth
+numVolumes_original = hSI.hStackManager.numVolumes;
+hSI.hStackManager.numVolumes = frames_per_depth;
+all_filenames = [];
+for iDepth = 1:length(ref_stack_z)
+    disp(['Acquiring volume ',num2str(iDepth),' of ',num2str(length(ref_stack_z))]);
+    % set z position
+    hSI.hMotors.moveSample([0 0 ref_stack_z(iDepth)]);
+
+    % set filename
+    % hSI.hScan2D.logFileStem = ['z_ref_',stack_filename_stem,'_',sprintf('%06d', ref_stack_z(iDepth)),'_'];
+    hSI.hScan2D.logFileStem = ['z_ref_',stack_filename_stem,'_',sprintf('%06d', iDepth),'_'];
+
+    hSI.startGrab;
+    % drawnow;
+    % pause(3);
+    % wait for grab to be complete
+    while strcmp(hSI.acqState,'grab')
+        drawnow();
+    end
 end
+
+
+disp(['Time taken = ',num2str(toc(x))]);
+
+
+% put settings back to start state
+hSI.hStackManager.numVolumes = 99999999;
+hSI.hChannels.loggingEnable=logInitialState;
+hSI.hScan2D.logFileStem = '';
+hSI.hScan2D.logFilePath = '';
+% set motor to where it was
+hSI.hMotors.moveSample([0 0 0]);
+
